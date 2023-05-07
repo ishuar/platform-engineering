be78d05083fbe8833ff5bb0d6fdc980793fdb077
Ishan Sharma <is@cloudeteer.de>
2023-05-07 04:53:45 +0200

    fix: argocd helm releae and added cluster bootstrap application

--- terraform/argo-cd.tf
+++ terraform/argo-cd.tf
@@ -1,18 +1,39 @@
-resource "helm_release" "argo" {
-  name             = "argo-cd"
+resource "helm_release" "argocd" {
+  name             = "argocd"
   repository       = "https://argoproj.github.io/argo-helm"
   chart            = "argo-cd"
   version          = "5.32"
   create_namespace = true
-  namespace        = "argo-cd"
+  namespace        = "argocd"
+  atomic           = true
 
-  # atomix = true
 
-  # values = [
-  #   "${path.module}/argo-cd.yaml"
-  # ]
+  ## Why using file() with values -> https://github.com/hashicorp/terraform-provider-helm/issues/838
+  values = [
+    file("${path.module}/argo-cd.yaml")
+  ]
 
   depends_on = [
     module.management_cluster
+  ]
+}
+
+resource "helm_release" "argocd-bootstrap-app" {
+  name             = "argocd-apps"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argocd-apps"
+  version          = "1.0"
+  create_namespace = true
+  namespace        = "argocd"
+  atomic           = true
+
+
+  ## Why using file() with values -> https://github.com/hashicorp/terraform-provider-helm/issues/838
+  values = [
+    file("${path.module}/argocd-apps.yaml")
+  ]
+
+  depends_on = [
+    helm_release.argocd
   ]
 }
